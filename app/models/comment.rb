4483b648112f08f010cb7c01ba833b5bfde4e346
Damian <36140669+kowalczykDamian@users.noreply.github.com>
2019-01-21 08:13:27 +0100

    Modified layout details.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :quote
 
   # TODO display errors msg for this validation
-  validates :nick, presence: true, length: { minimum: 3 }
+  validates :nick, presence: true, length: { minimum: 3, maximum: 25 }
 end
